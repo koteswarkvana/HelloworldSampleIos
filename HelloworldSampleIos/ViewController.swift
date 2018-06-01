b9d8e43ce3e2ea34b45283ca326f6b8ffde56984
Koteswarao <kotesh@kvana.com>
2018-06-01 15:21:27 +0530

    button event listener.

--- HelloworldSampleIos/ViewController.swift
+++ HelloworldSampleIos/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var result = 0
+    var increseButtoncount = ""
+    @IBOutlet var label_text: UILabel!
+    @IBAction func bt_click(_ sender: UIButton) {
+//        label_text.text = "Hi"
+        result += 1;
+        increseButtoncount = "Button clicked " + String(result)  + " time";
+        if result != 1 {
+        increseButtoncount += "s.";
+        }
+        label_text.text = increseButtoncount
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
